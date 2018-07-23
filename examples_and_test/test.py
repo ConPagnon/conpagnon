b8921441fe969831a8e14a6d4b7767fa2f911c64
Dhaif BEKHA <bekha.dhaif@gmail.com>
2018-07-23 17:35:27 +0200

    Machine learning stuff on language and other cognitive domain data.

--- examples_and_test/test.py
+++ examples_and_test/test.py
@@ -6,15 +6,14 @@ from machine_learning import classification
 from connectivity_statistics import parametric_tests
 from plotting import display
 from data_handling import data_management
+from utils.folders_and_files_management import load_object
 import os
-from connectivity_statistics.regression_analysis_model import one_way_anova, one_way_anova_network
-import statsmodels.api as sm
-import pandas as pd
-import statsmodels.stats.multicomp as multi
-from scipy import stats
 import numpy as np
-from statsmodels.stats.multitest import multipletests
-
+import pandas as pd
+from nilearn.connectome import sym_matrix_to_vec
+from sklearn.model_selection import LeaveOneOut, StratifiedShuffleSplit, KFold, GridSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.svm import SVR
 # Reload all module
 importlib.reload(data_management)
 importlib.reload(atlas)
@@ -27,40 +26,78 @@ importlib.reload(data_architecture)
 importlib.reload(dictionary_operations)
 
 
-groups = ['impaired_language', 'non_impaired_language', 'controls']
-kinds = ['tangent', 'partial correlation', 'correlation']
-models = ['mean_homotopic']
-variables_in_model = ['langage_clinique']
-root_analysis_directory = '/media/db242421/db242421_data/ConPagnon_data/language_study_anova'
-# Put a name for the subjects columns
-behavioral_dataframe = data_management.read_csv(os.path.join(root_analysis_directory, 'behavioral_data.csv'))
-behavioral_dataframe = behavioral_dataframe.rename(columns={behavioral_dataframe.columns[0]: 'subjects'})
-# Shift index to be the subjects identifiers
-behavioral_dataframe = data_management.shift_index_column(
-    panda_dataframe=behavioral_dataframe,
-    columns_to_index=['subjects'])
-# Correction method
-correction_method = ['fdr_bh', 'bonferroni']
-# network list for model at the network level
-networks_list = ['DMN']
-# Type one error threshold
-alpha = 0.05
+data_directory = '/media/db242421/db242421_data/ConPagnon_data/patients_ACM_controls/dictionary'
+output_directory = '/media/db242421/db242421_data/ConPagnon_data/test'
+kind = 'tangent'
+# Load subjects matrices: just ACM lesion here
+Z_subjects_matrices = load_object(os.path.join(data_directory,
+                                               'z_fisher_transform_subjects_connectivity_matrices.pkl'))
 
-# One way anova: whole brain level
-one_way_anova(models=models,
-              groups=groups,
-              kinds=kinds,
-              correction_method=correction_method,
-              root_analysis_directory=root_analysis_directory,
-              variables_in_model=variables_in_model,
-              behavioral_dataframe=behavioral_dataframe)
+# Load behavioral data file
+behavioral_data = '/neurospin/grip/protocols/MRI/AVCnn_Dhaif_2018/regression_data/regression_data.xlsx'
+regression_data_file = data_management.read_excel_file(
+    excel_file_path=behavioral_data,
+    sheetname='cohort_functional_data')
 
-# One way anova: network level
-one_way_anova_network(root_analysis_directory=root_analysis_directory,
-                      groups=groups,
-                      kinds=kinds,
-                      networks_list=networks_list,
-                      models=models,
-                      behavioral_dataframe=behavioral_dataframe,
-                      variables_in_model=variables_in_model,
-                      correction_method=correction_method)
+# Load atlas ROIs labels for writing purpose
+# Atlas excel information file
+atlas_excel_file = '/media/db242421/db242421_data/atlas_AVCnn/atlas_version2.xlsx'
+sheetname = 'complete_atlas'
+atlas_information = pd.read_excel(atlas_excel_file, sheetname='complete_atlas')
+atlas_roi_labels = list(atlas_information['anatomical label'])
+
+# Select a subset of patients
+# Compute the connectivity matrices dictionary with factor as keys.
+group_by_factor_subjects_connectivity, population_df_by_factor, factor_keys =\
+    dictionary_operations.groupby_factor_connectivity_matrices(
+        population_data_file=behavioral_data,
+        sheetname='cohort_functional_data',
+        subjects_connectivity_matrices_dictionnary=Z_subjects_matrices,
+        groupes=['patients'], factors=['langage_clinique'], drop_subjects_list=None)
+
+# In this dataset, I have Impaired and non Impaired language
+groups = ['Non_impaired_language', 'Impaired_language']
+language_connectivity_dictionary = dict.fromkeys(groups)
+# Subjects connectivity matrices for both group
+language_connectivity_dictionary['Impaired_language'] = group_by_factor_subjects_connectivity['A']
+language_connectivity_dictionary['Non_impaired_language'] = group_by_factor_subjects_connectivity['N']
+# Subject list for both group
+impaired_language_subjects_list = list(language_connectivity_dictionary['Impaired_language'].keys())
+non_impaired_language_subjects_list = list(language_connectivity_dictionary['Non_impaired_language'].keys())
+# Subjects labels
+language_subjects_labels = np.hstack((np.zeros(len(non_impaired_language_subjects_list)),
+                                      np.ones(len(impaired_language_subjects_list))))
+# Stack matrices
+vectorized_connectivity_matrices = sym_matrix_to_vec(
+    np.array([language_connectivity_dictionary[class_name][s][kind]
+              for class_name in groups
+              for s in language_connectivity_dictionary[class_name].keys()]),
+    discard_diagonal=True)
+
+# Run SVM classification
+from sklearn import svm
+# Leave one out Cross validation scheme
+loo = LeaveOneOut()
+# Stratified cross validation scheme
+sss = StratifiedShuffleSplit(n_splits=10000)
+# K-fold evaluation scheme
+kfold = KFold(n_splits=5, shuffle=True)
+# SVM kernel
+kernel = 'linear'
+C = 1
+# Support Vector Classification object
+svc = svm.SVC(C=C, kernel=kernel)
+
+# Compute classification score between impaired language
+# and non impaired language groups
+svm_scores = cross_val_score(estimator=svm.SVC(C=1, kernel='linear'),
+                             X=vectorized_connectivity_matrices,
+                             y=language_subjects_labels,
+                             cv=sss, scoring='accuracy')
+
+mean_accuracy = round(np.mean(svm_scores), 2)*100
+std_accuracy = round(np.std(svm_scores), 2)*100
+
+print('Mean prediction accuracy : {}%'.format(mean_accuracy))
+
+# Feature ranking and elimination
