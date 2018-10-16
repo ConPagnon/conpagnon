756d4894ded9b3155ec89fde58e79c9602a10916
Dhaif BEKHA <bekha.dhaif@gmail.com>
2018-10-16 18:23:44 +0200

    Build tables result for stats. The joint correction seem ok. I Should double check the results and put the code in function.

--- examples_and_test/build_results_tables.py
+++ examples_and_test/build_results_tables.py
@@ -40,7 +40,8 @@ whole_brain_models = ['mean_connectivity', 'mean_homotopic', 'mean_contralesiona
 variable_to_read = ['wisc_irp_zscores', 'Sexe[T.M]', 'lesion_normalized']
 
 network_name = ['DMN', 'Executive', 'Language', 'MTL', 'Primary_Visual',
-                'Salience', 'Secondary_Visual', 'Sensorimotor', 'Visuospatial']
+                'Salience', 'Secondary_Visual', 'Sensorimotor', 'Visuospatial',
+                'Basal_Ganglia', 'Auditory', 'Precuneus']
 
 # Network level model
 for variable in variable_to_read:
@@ -92,17 +93,50 @@ for model in whole_brain_models:
 
 
 # Model joint correction
+def _flatten(values):
+    if isinstance(values, np.ndarray):
+        yield values.flatten()
+    else:
+        for value in values:
+            yield from _flatten(value)
+
+def _unflatten(flat_values, prototype, offset):
+    if isinstance(prototype, np.ndarray):
+        shape = prototype.shape
+        new_offset = offset + np.product(shape)
+        value = flat_values[offset:new_offset].reshape(shape)
+        return value, new_offset
+    else:
+        result = []
+        for value in prototype:
+            value, offset = _unflatten(flat_values, value, offset)
+            result.append(value)
+        return result, offset
+
+def flatten(values):
+    # flatten nested lists of np.ndarray to np.ndarray
+    return np.concatenate(list(_flatten(values)))
+
+
+def unflatten(flat_values, prototype):
+    # unflatten np.ndarray to nested lists with structure of prototype
+    result, offset = _unflatten(flat_values, prototype, 0)
+    assert(offset == len(flat_values))
+    return result
+
+
 root_analysis_directory = '/media/db242421/db242421_data/ConPagnon_data/patients_behavior_ACM_test/' \
                           'pc1_language_zscores/regression_analysis'
 kinds = ['correlation', 'partial correlation', 'tangent']
-models = ['intra', 'ipsi_intra', 'contra_intra']
+models = ['intra', 'ipsi_intra', 'contra_intra', 'intra_homotopic']
 networks = network_name
 correction_methods = ['fdr_bh', 'bonferroni']
 alpha = 0.05
 
 import numpy as np
 from statsmodels.stats.multitest import multipletests
-
+from pathlib import Path
+import itertools
 # If networks is None, the models are whole brain models,
 # so we jus loop over whole brain model at the root of
 # the analysis directory
@@ -146,14 +180,74 @@ for kind in kinds:
                                 index=False)
 
     else:
-        # Stack over all network and models
-        all_network_models_p_values = []
+        network_models_dictionary = dict.fromkeys(models)
+        all_models_p_values = []
         for model in models:
+            network_model_p_values = []
             for network in network_name:
-                model_network_df = pd.read_csv(os.path.join(root_analysis_directory, kind,
-                                                            network, model + '_parameters.csv'))
-                all_network_models_p_values.append(model_network_df['p_value'])
+                model_network_df_path = Path(os.path.join(root_analysis_directory, kind, network,
+                                             model + '_parameters.csv'))
+                if model_network_df_path.exists():
 
-        # Convert list of p values to array
-        all_network_models_p_values_array = np.array(all_network_models_p_values)
+                    model_network_df = pd.read_csv(os.path.join(root_analysis_directory, kind,
+                                                                network, model + '_parameters.csv'))
+                    network_model_p_values.append(model_network_df['p_value'])
 
+                else:
+                    pass
+            # Convert to array
+            network_model_p_values_array = np.array(network_model_p_values)
+            # Fill the dictionary of the model
+            network_models_dictionary[model] = {'p values array': network_model_p_values_array,
+                                                'p values flat array': network_model_p_values_array.flatten(),
+                                                'number of networks': network_model_p_values_array.shape[0],
+                                                'number of variables': network_model_p_values_array.shape[1]}
+
+            # Append the network array of the model
+            all_models_p_values.append(network_model_p_values_array)
+
+        # convert it to array
+        all_models_p_values_array = np.array(all_models_p_values)
+        list_of_p_values = []
+
+        for i in range(len(models)):
+            list_of_p_values.append(all_models_p_values_array[i])
+        # Flatten the nested list of p values
+        all_models_p_values_list_flat = flatten(values=list_of_p_values)
+
+        for correction in correction_methods:
+            _, corrected_p_values, _, _ = multipletests(pvals=all_models_p_values_list_flat,
+                                                        alpha=alpha,
+                                                        method=correction)
+            # Rebuild the nested list of p values
+            corrected_p_values_unflatten = unflatten(corrected_p_values, list_of_p_values)
+
+            # Write the corrected p values in corresponding CSV
+            for model in models:
+                for network in network_name:
+                    model_network_df_path = Path(os.path.join(root_analysis_directory, kind, network,
+                                                              model + '_parameters.csv'))
+                    if model_network_df_path.exists():
+                        model_network_df = pd.read_csv(os.path.join(root_analysis_directory, kind,
+                                                                    network, model + '_parameters.csv'))
+                        # Append the corrected p value column
+                        model_network_df[correction + "corrected_pvalues"] = \
+                            corrected_p_values_unflatten[models.index(model)][network_name.index(network), ...]
+
+                        # Overwrite the dataframe
+                        model_network_df.to_csv(os.path.join(root_analysis_directory, kind, network,
+                                                     model + '_parameters_test.csv'),
+                                        index=False)
+
+                    else:
+                        pass
+
+
+
+
+
+a = list_of_p_values
+b = flatten(a)
+
+
+c = unflatten(b, a)
