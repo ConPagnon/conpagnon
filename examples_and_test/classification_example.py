f5c6abb79f34b0860e2e782a10d838d2dee03fb7
Dhaif Bekha <bekha.dhaif@gmail.com>
2018-10-21 17:57:41 +0100

    Update example
    Signed-off-by: Dhaif Bekha <bekha.dhaif@gmail.com>

--- examples_and_test/classification_example.py
+++ examples_and_test/classification_example.py
@@ -7,28 +7,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.backends.backend_pdf import PdfPages
 import os
+from plotting import display
 
-save_figures_dir = '/media/db242421/db242421_data/ConPagnon_data/features_identification_results/LesionFlip_controls'
+save_figures_dir = 'C://Users//Dhaïf//Documents//prez royaumont'
 
 # Load the connectivity matrices dictionary
 subjects_connectivity_matrices = load_object(os.path.join(save_figures_dir,
-                                                          'connectivity_matrices_LesionFlip_controls.pkl'))
+                                                          'connectivity_matrices_patients_controls.pkl'))
+
+
+# Plot all times series
+for group in subjects_connectivity_matrices.keys():
+    subject_list = list(subjects_connectivity_matrices[group].keys())
+    for subject in subject_list:
+        matrix = subjects_connectivity_matrices[group][subject]['correlation']
+        plt.figure()
+        display.plot_matrix(matrix=matrix, mpart='all')
+        plt.savefig(os.path.join(save_figures_dir, group + 'subject_' + str(subject_list.index(subject))
+                                 + '.png'))
 
 
 # Class name, i.e the name of the groups to differentiate
-class_names = ['LesionFlip', 'controls']
+class_names = ['patients', 'controls']
 # Labels vectors: 1 for the first class, -1 for the second
 class_labels = np.hstack((np.zeros(len(subjects_connectivity_matrices[class_names[0]].keys())),
                           np.ones(len(subjects_connectivity_matrices[class_names[1]].keys()))))
 
 n_subjects = len(class_labels)
 
-# I want to see if just homotopic connectivity make a good marker
-homotopic_roi_indices = np.array([
-    (1, 0), (2, 3), (4, 5), (6, 7), (8, 11), (9, 10), (13, 12), (14, 15), (16, 17), (18, 19), (20, 25),
-    (21, 26), (22, 29), (23, 28), (24, 27), (30, 31), (32, 33), (35, 34), (36, 37), (38, 39), (44, 40),
-    (41, 45), (42, 43), (46, 49), (47, 48), (50, 53), (53, 54), (54, 57), (55, 56), (58, 61), (59, 60),
-    (62, 63), (64, 65), (66, 67), (68, 69), (70, 71)])
 
 # Stratified Shuffle and Split cross validation:
 cv = StratifiedShuffleSplit(n_splits=10000,
@@ -45,10 +51,11 @@ mean_scores = []
 mean_score_dict = {}
 for metric in metrics:
     print('Evaluate classification performance on {}...'.format(metric))
-    features = np.array([subjects_connectivity_matrices[group][subject][metric][
-                             homotopic_roi_indices[:, 0],
-                             homotopic_roi_indices[:, 1]] for group in class_names
-                         for subject in subjects_connectivity_matrices[group].keys()])
+    features = sym_matrix_to_vec(np.array([subjects_connectivity_matrices[group][subject][metric]
+                                           for group in class_names
+                                           for subject in subjects_connectivity_matrices[group].keys()],
+                                          ), discard_diagonal=True)
+    print(features.shape)
     cv_scores = cross_val_score(estimator=svc, X=features,
                                 y=class_labels, cv=cv,
                                 scoring='accuracy')
@@ -63,7 +70,7 @@ save_object(object_to_save=mean_score_dict,
             filename=class_names[0] + '_' + class_names[1] + '.pkl')
 
 # bar plot of classification results for the different metrics
-with PdfPages(os.path.join(save_figures_dir, 'lesion_flip_controls_homotopic.pdf')) as pdf:
+with PdfPages(os.path.join(save_figures_dir, 'patients_controls.pdf')) as pdf:
 
     plt.figure()
     sns.barplot(x=list(mean_score_dict.keys()), y=list(mean_score_dict.values()))
