c367c5a1fc112d00050527c7cddf74d0d4a231e9
Dhaif BEKHA <bekha.dhaif@gmail.com>
2018-11-07 15:07:59 +0100

    Code for features identification using one versus rest, with the same algorithm used in discriminative connection identification in binary case.

--- examples_and_test/one_vs_the_rest_features_identification.py
+++ examples_and_test/one_vs_the_rest_features_identification.py
@@ -14,7 +14,7 @@ from plotting.display import plot_matrix
 from utils.folders_and_files_management import save_object
 from matplotlib.backends.backend_pdf import PdfPages
 from nilearn.connectome import sym_matrix_to_vec
-
+from machine_learning.features_indentification import one_against_all_bootstrap, one_against_all_permutation_bootstrap
 
 # Atlas set up
 atlas_folder = '/media/db242421/db242421_data/ConPagnon_data/atlas/atlas_reference'
@@ -44,8 +44,6 @@ subjects_connectivity_matrices = load_object(os.path.join(data_folder,
 save_figures = '/media/db242421/db242421_data/ConPagnon_data/language_study_ANOVA_ACM_controls/' \
                'discriminative_connection_identification/one_vs_the_rest'
 
-
-#class_names = list(subjects_connectivity_matrices.keys())
 class_names = ['controls', 'impaired_language', 'non_impaired_language']
 metric = 'tangent'
 
@@ -69,7 +67,9 @@ class_labels = np.hstack((1*np.ones(len(subjects_connectivity_matrices[class_nam
 
 from sklearn.svm import LinearSVC
 from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
-from sklearn.preprocessing import label_binarize
+
+
+
 
 svc = LinearSVC()
 one_vs_the_rest_classifier = OneVsRestClassifier(estimator=svc)
@@ -112,3 +112,84 @@ with PdfPages(os.path.join(save_figures, 'one_vs_the_rest.pdf')) as pdf:
                         node_color=labels_colors)
         pdf.savefig()
         plt.show()
+
+from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit
+
+sss = StratifiedShuffleSplit(n_splits=10000)
+
+scores = cross_val_score(estimator=one_vs_the_rest_classifier, X=vectorized_connectivity_matrices,
+                         y=class_labels, cv=sss, verbose=1, n_jobs=4)
+mean_score = scores.mean()
+std_score = scores.std()
+
+print('One vs All classification accuracy: {} % +- {} %'.format(mean_score*100,
+                                                                std_score*100))
+
+n_subjects = vectorized_connectivity_matrices.shape[0]
+bootstrap_number = 100
+n_permutations = 10
+n_classes = len(np.unique(class_labels))
+# Indices to bootstrap
+indices = np.arange(n_subjects)
+# Generate a matrix containing all bootstrapped indices
+bootstrap_matrix = np.random.choice(a=indices, size=(bootstrap_number, n_subjects),
+                                    replace=True)
+
+# Generate a permuted class labels array
+class_labels_permutation_matrix = np.array([np.random.permutation(class_labels)
+                                            for n in range(n_permutations)])
+
+bootstrap_array_perm = np.random.choice(a=indices,
+                                        size=(n_permutations, bootstrap_number,
+                                              n_subjects),
+                                        replace=True)
+
+bootstrap_weights = one_against_all_bootstrap(features=vectorized_connectivity_matrices,
+                                                        class_labels=class_labels,
+                                                        bootstrap_array_indices=bootstrap_matrix,
+                                                        n_cpus_bootstrap=8,
+                                                        verbose=1)
+
+normalized_mean_weight = bootstrap_weights.mean(axis=0)/bootstrap_weights.std(axis=0)
+
+save_object(object_to_save=normalized_mean_weight, saving_directory=save_figures,
+            filename='normalized_mean_weight.pkl')
+
+null_distribution = one_against_all_permutation_bootstrap(features=vectorized_connectivity_matrices,
+                                                          class_labels_perm=class_labels_permutation_matrix,
+                                                          bootstrap_array_perm=bootstrap_array_perm,
+                                                          n_classes=3,
+                                                          n_permutations=n_permutations,
+                                                          n_cpus_bootstrap=8,
+                                                          verbose_bootstrap=1,
+                                                          verbose_permutation=1)
+
+# Save the null distribution to avoid
+save_object(object_to_save=null_distribution, saving_directory=save_figures,
+            filename='null_distribution.pkl')
+
+# Rebuild a symmetric array from normalized mean weight vector for each class
+normalized_mean_weight_array = np.zeros((n_classes, n_nodes, n_nodes))
+for group in range(n_classes):
+
+    normalized_mean_weight_array[group, ...] = array_rebuilder(normalized_mean_weight[group, ...],
+                                                   'numeric', diagonal=np.zeros(n_nodes))
+# Find top features for each class
+all_classes_normalized_mean_weight_array_top_features = np.zeros((n_classes, n_nodes, n_nodes))
+all_classes_top_weights = []
+all_classes_top_coefficients_indices = []
+all_classes_top_weight_labels = []
+for group in range(n_classes):
+
+    normalized_mean_weight_array_top_features, top_weights, top_coefficients_indices, top_weight_labels = \
+        find_top_features(normalized_mean_weight_array=normalized_mean_weight_array[group, ...],
+                          labels_regions=labels_regions)
+    all_classes_normalized_mean_weight_array_top_features[group, ...] = normalized_mean_weight_array_top_features
+    all_classes_top_weights.append(top_weights)
+    all_classes_top_coefficients_indices.append(top_coefficients_indices)
+    all_classes_top_weight_labels.append(top_weight_labels)
+
+# Convert list to array for later purposes
+all_classes_top_weights = np.array(all_classes_top_weights)
+all_classes_top_coefficients_indices = np.array(all_classes_top_coefficients_indices)
+all_classes_top_weight_labels = np.array(all_classes_top_weight_labels)
