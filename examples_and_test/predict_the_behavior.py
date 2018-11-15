181416f2b78e7c1b776507293f504d387eebcfb3
Dhaif BEKHA <bekha.dhaif@gmail.com>
2018-11-15 10:26:00 +0100

    Prediction for intra-network homotopic brain connection between non-impaired language and controls. Update path for neurospin.

--- examples_and_test/predict_the_behavior.py
+++ examples_and_test/predict_the_behavior.py
@@ -1,23 +1,15 @@
 import importlib
 from data_handling import atlas, data_architecture, dictionary_operations
 from utils import folders_and_files_management
+from utils.array_operation import array_rebuilder
 from computing import compute_connectivity_matrices as ccm
-from sklearn import covariance
 from machine_learning import classification
 from connectivity_statistics import parametric_tests
 from plotting import display
-from matplotlib.backends import backend_pdf
 from data_handling import data_management
 import os
-import time
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from math import ceil
-from connectivity_statistics import regression_analysis_model
-from plotting.display import t_and_p_values_barplot
-from scipy.stats import ttest_rel
-from matplotlib.backends.backend_pdf import PdfPages
 from sklearn.model_selection import cross_val_score, StratifiedShuffleSplit, LeaveOneOut, GridSearchCV
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
@@ -33,14 +25,13 @@ importlib.reload(data_architecture)
 importlib.reload(dictionary_operations)
 
 # Some path
-root_directory = "/media/db242421/db242421_data/ConPagnon_data/language_study_ANOVA_ACM_controls"
-save_in = '/media/db242421/db242421_data/ConPagnon_data/language_study_ANOVA_ACM_controls/classification'
+root_directory = "/neurospin/grip/protocols/MRI/AVCnn_Dhaif_2018/network_classification"
+save_in = '/neurospin/grip/protocols/MRI/AVCnn_Dhaif_2018/network_classification'
 kinds = ['correlation', 'partial correlation', 'tangent']
-network_name = [
-                'Primary_Visual',
-                'Salience',
-    'Precuneus']
-
+network_name = ['DMN', 'Auditory', 'Executive',
+                         'Language', 'Basal_Ganglia', 'MTL',
+                         'Salience', 'Sensorimotor', 'Visuospatial',
+                         'Primary_Visual', 'Precuneus', 'Secondary_Visual']
 subjects_connectivity_matrices = folders_and_files_management.load_object(
     full_path_to_object=os.path.join(root_directory,
                                      "dictionary/z_fisher_transform_subjects_connectivity_matrices.pkl")
@@ -130,19 +121,111 @@ for network in network_name:
 folders_and_files_management.save_object(
     object_to_save=save_network_parameters,
     saving_directory=os.path.join(save_in,
-                               'non_impaired_language_controls/Network classification'),
+                                  'non_impaired_language_controls/Network classification'),
     filename='best_params_and_accuracy_networks_intra_homotopic_until_1.pkl')
 
 folders_and_files_management.save_object(
     object_to_save=homotopic_intra_network_connectivity,
     saving_directory=os.path.join(save_in,
-                               'non_impaired_language_controls/Network classification'),
+                                  'non_impaired_language_controls/Network classification'),
     filename='homotopic_networks_dictionary.pkl'
 )
 
 folders_and_files_management.save_object(
     object_to_save=features_labels,
     saving_directory=os.path.join(save_in,
-                               'non_impaired_language_controls/Network classification'),
+                                  'non_impaired_language_controls/Network classification'),
     filename='homotopic_networks_features_labels.pkl'
 )
+
+
+# Identify important brain connection in each network
+# Atlas set up
+atlas_folder = '/neurospin/grip/protocols/MRI/AVCnn_Dhaif_2018/network_classification/atlas_AVCnn'
+atlas_name = 'atlas4D_2.nii'
+labels_text_file = '/neurospin/grip/protocols/MRI/AVCnn_Dhaif_2018/network_classification/' \
+                   'atlas_AVCnn/atlas4D_2_labels.csv'
+colors = ['navy', 'sienna', 'orange', 'orchid', 'indianred', 'olive',
+          'goldenrod', 'turquoise', 'darkslategray', 'limegreen', 'black',
+          'lightpink']
+# Number of regions in each user defined networks
+networks = [2, 10, 2, 6, 10, 2, 8, 6, 8, 8, 6, 4]
+# Atlas path
+# Read labels regions files
+atlas_nodes, labels_regions, labels_colors, n_nodes = atlas.fetch_atlas(
+    atlas_folder=atlas_folder,
+    atlas_name=atlas_name,
+    network_regions_number=networks,
+    colors_labels=colors,
+    labels=labels_text_file,
+    normalize_colors=True)
+
+# Load dictionary containing the best C parameters
+best_parameters = folders_and_files_management.load_object(
+    full_path_to_object=os.path.join(
+        save_in,
+        'best_params_and_accuracy_networks_intra_homotopic.pkl')
+)
+from nilearn.connectome import vec_to_sym_matrix
+from machine_learning.features_indentification import compute_weight_distribution, \
+    features_weights_parametric_correction, features_weights_max_t_correction
+connectivity_matrices = homotopic_intra_network_connectivity
+network_name = ['Auditory', 'Basal_Ganglia', 'DMN',
+                'Executive', 'Language', 'Precuneus',
+                'MTL', 'Primary_Visual',
+                'Secondary_Visual',
+                'Sensorimotor', 'Salience', 'Visuospatial']
+network_classification_results = dict.fromkeys(network_name)
+correction = 'max_t'
+for network in network_name:
+    print('Identify important connection for the {} network'.format(network))
+    # Compute mean network matrices
+    first_class_mean_matrix = vec_to_sym_matrix(
+        vec=np.array([intra_network_connectivity_dict[groups[0]][s][kind][network]['network array']
+                      for s in list(intra_network_connectivity_dict[groups[0]].keys())]),
+        diagonal= np.zeros((len(list(intra_network_connectivity_dict[groups[0]].keys())),
+                            network_dict[network]['number of rois']))).mean(axis=0)
+    second_class_mean_matrix = vec_to_sym_matrix(
+        vec=np.array([intra_network_connectivity_dict[groups[1]][s][kind][network]['network array']
+                      for s in list(intra_network_connectivity_dict[groups[1]].keys())]),
+        diagonal= np.zeros((len(list(intra_network_connectivity_dict[groups[1]].keys())),
+                            network_dict[network]['number of rois']))).mean(axis=0)
+
+    vectorized_connectivity_matrices = homotopic_intra_network_connectivity[network]
+    class_labels = features_labels
+
+    network_classification_weight, network_weight_distribution = compute_weight_distribution(
+        vectorized_connectivity_matrices=vectorized_connectivity_matrices,
+        bootstrap_number=500,
+        n_permutations=10000,
+        class_labels=class_labels,
+        C=best_parameters[network]['Best C'],
+        n_cpus_bootstrap=16,
+        verbose_permutations=0,
+        verbose_bootstrap=0
+    )
+
+    #  network_p_values_corrected = features_weights_parametric_correction(
+    #     null_distribution_features_weights=network_weight_distribution,
+    #    normalized_mean_weight=network_classification_weight,
+    #    method=correction,
+    #    alpha=.05
+    # )
+    sorted_null_maximum_dist, sorted_null_minimum_dist, p_values_max, p_values_min = \
+        features_weights_max_t_correction(null_distribution_features_weights=network_weight_distribution,
+                                          normalized_mean_weight=network_classification_weight)
+    # network_classification_results[network] = {'network_features_weight': network_classification_weight,
+    #                                           'network_weight_distribution': network_weight_distribution,
+    #                                           'p_values_corrected': network_p_values_corrected}
+    network_classification_results[network] = {'sorted_null_maximum_dist': sorted_null_maximum_dist,
+                                               'sorted_null_minimum_dist': sorted_null_minimum_dist,
+                                               'p_values_max': p_values_max,
+                                               'p_values_min': p_values_min}
+
+folders_and_files_management.save_object(
+    object_to_save=network_classification_results,
+    saving_directory=save_in,
+    filename=correction + '_intra_homotopic_connection_identification.pkl'
+)
+
+# Plot results for intra network homotopic connection for each network
