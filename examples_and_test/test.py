e62005cad4b18c4f1e71189a87278f99262895c0
Dhaif BEKHA <bekha.dhaif@gmail.com>
2018-11-27 17:03:39 +0100

    In test: Compute correlation between connectivity composite score for each group in the analysis.

--- examples_and_test/test.py
+++ examples_and_test/test.py
@@ -29,107 +29,116 @@ importlib.reload(dictionary_operations)
 
 # Build a giant dataset: all connectitivity type in columns, and subjects id in lines
 
-root_data_directory = '/media/db242421/db242421_data/ConPagnon_data/patients_behavior/language_score/tangent'
+root_data_directory = '/media/db242421/db242421_data/ConPagnon_data/language_study_ANOVA_ACM_controls'
 
 
 models = ['contra_intra', 'ipsi_intra', 'intra_homotopic']
 networks = ['DMN', 'Executive', 'Language',  'MTL',
            'Salience', 'Sensorimotor', 'Visuospatial',
            'Primary_Visual', 'Secondary_Visual']
-data_to_merge = []
 
-for network in networks:
-    for model in models:
-        # Read the model dataframe
-        raw_model_data = pd.read_csv(os.path.join(root_data_directory, network, 'patients_' + model + '_' + network +
-                                              '_connectivity.csv'))
+groups = ['impaired_language', 'non_impaired_language', 'controls']
+kind = 'tangent'
+for group in groups:
+    data_to_merge = []
+    for network in networks:
+        for model in models:
+            # Read the model dataframe
+            raw_model_data = pd.read_csv(os.path.join(root_data_directory, kind, network, group +
+                                                      '_' + model + '_' + network + '_connectivity.csv'))
 
-        # Shift index to subjects nip
-        model_data = data_management.shift_index_column(panda_dataframe=raw_model_data,
-                                                        columns_to_index=['subjects'])
-        # rename the columns
-        model_data.columns = [model + '_' + network + '_connectivity']
-        # Stack the dataframe
-        data_to_merge.append(model_data)
-# build the giant dataframe
-all_models_data = data_management.merge_list_dataframes(data_to_merge)
-all_models_data_T = all_models_data.T
-all_models_data_T.to_csv('/media/db242421/db242421_data/ConPagnon_data/test/all_connectivity_T.csv')
-all_models_data.to_csv('/media/db242421/db242421_data/ConPagnon_data/test/all_connectivity.csv')
+            # Shift index to subjects nip
+            model_data = data_management.shift_index_column(panda_dataframe=raw_model_data,
+                                                            columns_to_index=['subjects'])
+            # rename the columns
+            model_data.columns = [model + '_' + network + '_connectivity']
+            # Stack the dataframe
+            data_to_merge.append(model_data)
+    # build the giant dataframe
+    all_models_data = data_management.merge_list_dataframes(data_to_merge)
+    all_models_data_T = all_models_data.T
+    all_models_data_T.to_csv(os.path.join(root_data_directory,
+                                          'correlation_connectivity_scores',  group + '_all_connectivity_T.csv'))
+    all_models_data.to_csv(os.path.join(root_data_directory,
+                                        'correlation_connectivity_scores', group + '_all_connectivity.csv'))
 
-from scipy.stats import pearsonr
-# Some statistic test
-alpha = 0.05
+    # Some statistic test
+    alpha = 0.05
 
-R_mat = all_models_data.corr()
-labels = list(R_mat.index)
-df = 25
-# Convert in T mat
-T_mat = (np.sqrt(df) * np.abs(R_mat)) / (np.sqrt(np.ones(R_mat.shape[0]) - R_mat ** 2))
-# compute p values
-P_mat = t.sf(np.abs(T_mat), df) * 2
+    R_mat = all_models_data.corr()
+    labels = list(R_mat.index)
+    # Degree of freedom: number of subjects - 2 (intercept, and one variable)
+    df = len(all_models_data.index) - 2
+    # Convert in T mat
+    T_mat = (np.sqrt(df) * np.abs(R_mat)) / (np.sqrt(np.ones(R_mat.shape[0]) - R_mat ** 2))
+    # compute p values
+    P_mat = t.sf(np.abs(T_mat), df) * 2
 
-# Vectorize the P matrix
+    from statsmodels.formula.api import ols
+    test = ols(formula='intra_homotopic_DMN_connectivity~intra_homotopic_Executive_connectivity',
+               data=all_models_data).fit()
+   # test.summary()
+    # Vectorize the P matrix
 
-P_mat_vec = sym_matrix_to_vec(P_mat, discard_diagonal=True)
+    P_mat_vec = sym_matrix_to_vec(P_mat, discard_diagonal=True)
 
+    P_mat_vec_corrected = multipletests(pvals=P_mat_vec, method='fdr_bh', alpha=alpha)[1]
 
-P_mat_vec_corrected = multipletests(pvals=P_mat_vec, method='fdr_bh', alpha=alpha)[1]
+    P_mat_corrected = vec_to_sym_matrix(P_mat_vec_corrected, diagonal=np.ones(R_mat.shape[0])*(1/np.sqrt(2)))
+    np.savetxt(fname=os.path.join(root_data_directory,
+                                  'correlation_connectivity_scores',  group + '_p_corrected.csv'),
+               X=P_mat_corrected,
+               delimiter=',', header="")
 
-P_mat_corrected = vec_to_sym_matrix(P_mat_vec_corrected, diagonal=np.ones(R_mat.shape[0])*(1/np.sqrt(2)))
-np.savetxt(fname='/media/db242421/db242421_data/ConPagnon_data/test/pcorrected.csv', X=P_mat_corrected,
-           delimiter=',', header="")
+    labels_color = ['goldenrod', 'indianred', 'sienna', 'lightpink',
+                    'turquoise', 'black', 'darkslategray',
+                    'orchid', 'limegreen']
+    labels_color = [webcolors.name_to_rgb(i) for i in labels_color]
 
+    # Re-index the matrices, sorted the rows and columns by connectivity type
+    sorted_labels = sorted(labels)
+    # new_sorted_labels = sorted_labels[9:18] + sorted_labels[18:27] + sorted_labels[27:36] + sorted_labels[0:9]
+    new_sorted_labels = sorted_labels[9:18] + sorted_labels[18:27] + sorted_labels[0:9]
 
-labels_color = ['goldenrod', 'indianred', 'sienna', 'lightpink', 'turquoise', 'black', 'darkslategray',
-                'orchid', 'limegreen']
-labels_color = [webcolors.name_to_rgb(i) for i in labels_color]
+    sorted_labels_colors = (1/255)*np.array(labels_color + labels_color + labels_color + labels_color)
+    new_index_order = np.array([labels.index(i) for i in new_sorted_labels])
 
-# Re-index the matrices, sorted the rows and columns by connectivity type
-sorted_labels = sorted(labels)
-#new_sorted_labels = sorted_labels[9:18] + sorted_labels[18:27] + sorted_labels[27:36] + sorted_labels[0:9]
-new_sorted_labels = sorted_labels[9:18] + sorted_labels[18:27] + sorted_labels[0:9]
+    # Re-index correlation matrix
+    R_mat = np.array(R_mat)
+    R_mat_reindex = R_mat[new_index_order, :]
+    R_mat_reindex = R_mat_reindex[:, new_index_order]
 
-sorted_labels_colors = (1/255)*np.array(labels_color + labels_color + labels_color + labels_color)
-new_index_order = np.array([labels.index(i) for i in new_sorted_labels])
+    # Re-index p corrected matrices
+    P_mat_corrected_reindex = P_mat_corrected[new_index_order, :]
+    P_mat_corrected_reindex = P_mat_corrected_reindex[:, new_index_order]
 
-# Re-index correlation matrix
-R_mat = np.array(R_mat)
-R_mat_reindex = R_mat[new_index_order, :]
-R_mat_reindex = R_mat_reindex[:, new_index_order]
+    with PdfPages(os.path.join(root_data_directory,
+                               'correlation_connectivity_scores',  group + '_fdr_bh_corrected.pdf')) as pdf:
 
-# Re-index p corrected matrices
-P_mat_corrected_reindex = P_mat_corrected[new_index_order, :]
-P_mat_corrected_reindex = P_mat_corrected_reindex[:, new_index_order]
+        plot_matrix(matrix=R_mat_reindex, mpart='lower',
+                    vmin=-1, vmax=1, horizontal_labels=new_sorted_labels,
+                    vertical_labels=new_sorted_labels, linecolor='black',
+                    title='Correlation between connectivity scores in {}'.format(group),
+                    labels_colors=sorted_labels_colors)
+        pdf.savefig()
+        plt.show()
 
-with PdfPages('/media/db242421/db242421_data/ConPagnon_data/test/P_mat_corrected_fdr_bh_wo_intra_' +
-              str(alpha) + '.pdf') as pdf:
+        plot_matrix(matrix=P_mat_corrected_reindex, mpart='lower',
+                    colormap='hot', vmin=0, vmax=alpha, horizontal_labels=new_sorted_labels,
+                    vertical_labels=new_sorted_labels, linecolor='black',
+                    title='Significant P values, alpha = {}'.format(alpha),
+                    labels_colors=sorted_labels_colors)
+        pdf.savefig()
+        plt.show()
 
-    plot_matrix(matrix=R_mat_reindex, mpart='all',
-                vmin=-1, vmax=1, horizontal_labels=new_sorted_labels,
-                vertical_labels=new_sorted_labels, linecolor='black',
-                title='Correlation between connectivity types in patients',
-                labels_colors=sorted_labels_colors)
-    pdf.savefig()
-    plt.show()
+        # Corresponding correlation matrix
+        mask = P_mat_corrected_reindex < alpha
+        R_mat_masked = np.multiply(mask, R_mat_reindex)
 
-    plot_matrix(matrix=P_mat_corrected_reindex, mpart='lower',
-                colormap='hot', vmin=0, vmax=alpha, horizontal_labels=new_sorted_labels,
-                vertical_labels=new_sorted_labels, linecolor='black',
-                title='Significant P values, alpha = {}'.format(alpha),
-                labels_colors=sorted_labels_colors)
-    pdf.savefig()
-    plt.show()
-
-    # Corresponding correlation matrix
-    mask = P_mat_corrected_reindex < alpha
-    R_mat_masked = np.multiply(mask, R_mat_reindex)
-
-    plot_matrix(matrix=R_mat_masked, mpart='lower',
-                vmin=-1, vmax=1, horizontal_labels=new_sorted_labels,
-                vertical_labels=new_sorted_labels, linecolor='black',
-                title='Significant correlation',
-                labels_colors=sorted_labels_colors)
-    pdf.savefig()
-    plt.show()
-
+        plot_matrix(matrix=R_mat_masked, mpart='lower',
+                    vmin=-1, vmax=1, horizontal_labels=new_sorted_labels,
+                    vertical_labels=new_sorted_labels, linecolor='black',
+                    title='Significant correlation',
+                    labels_colors=sorted_labels_colors)
+        pdf.savefig()
+        plt.show()
