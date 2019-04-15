42fb087bad82f56e7884cb2e3884b6a8bceb2cd9
Dhaif BEKHA <bekha.dhaif@gmail.com>
2019-04-15 17:03:55 +0200

    Small routine to plot in a simple way results of the TBSS analysis.

---
+++ examples_and_test/tbss_display.py
@@ -0,0 +1,59 @@
+"""
+ Created by Dhaif BEKHA, (dhaif.bekha@cea.fr).
+
+ """
+"""
+This script display in simple 
+manner the results of a TBSS
+analysis. Basically it thicken 
+the skeleton of the p values image and plot this 
+skeleton with the significant area circled 
+in the color of you're choice on top of the 
+FA skeleton.
+You will need a p-value corrected image,
+the mean_FA.nii.gz image, 
+the mean_FA_skeleton.nii.gz image, and
+the threshold you used 
+
+"""
+from subprocess import Popen, PIPE
+
+# Set path for plotting
+
+# The corrected p value
+corrected_p_values_image = "/media/db242421/db242421_data/DTI_TBSS_M2Ines/language_status/" \
+                           "tbss_language_profile_tfce_corrp_tstat1.nii.gz"
+# the thicken corrected p value image
+# The FA template
+mean_FA_image = "/media/db242421/db242421_data/DTI_TBSS_M2Ines/language_status/mean_FA.nii.gz"
+mean_FA_image_range = "0 0.6"
+# The FA template skeleton
+# The threshold you used in the analysis
+skeleton_threshold = str(0.3)
+mean_FA_skeleton_image = "/media/db242421/db242421_data/DTI_TBSS_M2Ines/language_status/mean_FA_skeleton.nii.gz"
+mean_FA_skeleton_color = "Green"
+mean_FA_skeleton_range = "{} 0.7".format(skeleton_threshold)
+
+# Filled corrected p values
+tbss_filled_image_color_map = "Red-Yellow"
+tbss_fill_output_image = "/media/db242421/db242421_data/DTI_TBSS_M2Ines/language_status/tbss_fill.nii.gz"
+
+tbss_fill = ["tbss_fill",
+             corrected_p_values_image,
+             "0.95",
+             mean_FA_image,
+             tbss_fill_output_image
+             ]
+tbss_fill_command = Popen(tbss_fill, stdout=PIPE)
+tbss_fill_output, tbss_fill_error = tbss_fill_command.communicate()
+
+display_results = ["fsleyes",
+                   mean_FA_image,
+                   "-dr", mean_FA_image_range,
+                   "-cm", mean_FA_skeleton_color,
+                   "-dr", mean_FA_skeleton_range,
+                   tbss_fill_output_image,
+                   "-cm", tbss_filled_image_color_map
+                   ]
+display_results_command = Popen(display_results, stdout=PIPE)
+display_results_output, display_results_error = display_results_command.communicate()
