006560a34e543ae9783eb35f66560a663ba2de6d
Dhaif BEKHA <bekha.dhaif@gmail.com>
2019-04-25 16:16:21 +0200

    Add a theme

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -13,12 +13,12 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
-
+import guzzle_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 
 project = 'ConPagnon'
-copyright = '2019, Dhaif BEKHA'
+copyright = '2016 - 2019, Dhaif BEKHA'
 author = 'Dhaif BEKHA'
 
 # The full version, including alpha/beta/rc tags
@@ -30,11 +30,12 @@ release = '2.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc'
-]
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc',
+              'guzzle_sphinx_theme']
+
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = [guzzle_sphinx_theme.html_theme_path()]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,9 +48,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'guzzle_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Guzzle theme options
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": project,
+}
