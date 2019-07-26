a8804f66aa677e27779a956fba1b774d2e79ca30
Dhaif BEKHA <bekha.dhaif@gmail.com>
2019-07-26 18:02:18 +0200

    Update.

--- examples_and_test/format_result_stats_tract.py
+++ examples_and_test/format_result_stats_tract.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import re
-
+import os
 """
 Format R results from statistical 
 analysis of bundle parameters from
@@ -8,30 +8,71 @@ NAIS cohort.
 
 """
 
+def _parse_line(line, rx_dict):
+    """
+
+    """
+    for key, rx in rx_dict.items():
+
+        match = rx.search(line)
+        if match:
+            return key, match
+
+    return None, None
+
+
 stat_results_directory = "/media/db242421/Samsung_T5/Work/Neurospin/AVCnn/Ines_2018/stats_tracts_2"
-model_name = "_Ipsi_contra_Parole_Zscore_laure"
-parameter = ["FA"]
-side = ["ipsi", "contra"]
-bundle_name = ["AF", "MLF", "SLF"]
+model_names = ["_Ipsi_Contra_Parole_Zscore_laure"]
+parameters = ["FA"]
+sides = ["ipsi", "contra"]
+bundle_names = ["AF", "MLF", "SLF"]
 
 variables = ["Lesion_ratio"]
 
-one_result_file = "/media/db242421/Samsung_T5/Work/Neurospin/AVCnn/Ines_2018/stats_tracts_2/" \
-                  "AF_contra_FA__Ipsi_Contra_Parole_Zscore_laure.txt"
+for parameter in parameters:
+    for side in sides:
+        for model in model_names:
+            for bundle in bundle_names:
+                # Build the regular expression dictionary to
+                # search for in the result text file
+                rx_dict = {"Lesion_ratio": re.compile(r"Lesion_ratio\s\s\s[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?.*$"),
+                           bundle + "_" + side + "_" + parameter:
+                               re.compile(r"{}\s\s\s[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?.*$".format(
+                                   bundle + "_" + side + "_" + parameter)),
+                           "Lesion_ratio_": re.compile(r"Lesion_ratio\s\s\s\s[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?.*$")}
 
-with open(one_result_file) as result_file:
-    result_file_content = result_file.read()
-    print(result_file_content)
+                # Open the text file containing the
+                # result for the current model
+                bundle_model_result = os.path.join(stat_results_directory, bundle + "_" + side + "_" + parameter +
+                                                   "_" + model + ".txt")
 
-lesion_ratio_pattern = r"Lesion_ratio\s\s\s[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?.*$"
+                # read the result txt file
+                with open(bundle_model_result) as bundle_result_file:
+                    # Go through each line, to have a match for the
+                    # regular expression
+                    for line in bundle_result_file:
+                        key, match = _parse_line(line, rx_dict=rx_dict)
+                        if key == "Lesion_ratio":
+                            model_bundle_lesion_ratio = match.group()
+                            t_lesion_ratio, p_lesion_ratio = model_bundle_lesion_ratio.split()[3:5]
+                            # Format the t and p couple: t={}, p={}
+                            lesion_ratio_data = "t={}, p={}".format(t_lesion_ratio, p_lesion_ratio)
+                        if key == "Lesion_ratio_":
+                            model_bundle_lesion_ratio = match.group()
+                            t_lesion_ratio, p_lesion_ratio = model_bundle_lesion_ratio.split()[3:5]
+                            # Format the t and p couple: t={}, p={}
+                            lesion_ratio_data = "t={}, p={}".format(t_lesion_ratio, p_lesion_ratio)
+                        if key == bundle + "_" + side + "_" + parameter:
+                            model_bundle_parameter = match.group()
+                            t_bundle_parameter, p_bundle_parameter = model_bundle_parameter.split()[3:5]
+                            # Format the t and p couple: t={}, p={}
+                            model_bundle_parameter_data = "t={}, p={}".format(t_bundle_parameter,
+                                                                              p_bundle_parameter)
 
-test = re.compile(lesion_ratio_pattern)
 
-with open(one_result_file) as result_file:
-    for line in result_file:
-        match = test.search(line)
-        if match:
-            match_result = line
-            parse_line = match_result.split()[-2:]
+
+
+
+
 
 
